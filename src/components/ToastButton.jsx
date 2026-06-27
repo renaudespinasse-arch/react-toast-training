4988c42f3d12cb16d5fb8b658493007267ea87f5
renaudespinasse-arch <renaud.espinasse@gmail.com>
2026-06-27 21:34:04 +0200

    refactor: pass toast options as object

--- src/components/ToastButton.jsx
+++ src/components/ToastButton.jsx
@@ -1,6 +1,14 @@
 function ToastButton({ label, message, type, duration, onShowToast }) {
   return (
-    <button onClick={() => onShowToast(message, type, duration)}>
+    <button
+      onClick={() =>
+        onShowToast({
+          message,
+          type,
+          duration,
+        })
+      }
+    >
       {label}
     </button>
   );
